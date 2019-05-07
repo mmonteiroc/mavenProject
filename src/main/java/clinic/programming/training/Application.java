6dbb44da79e7360cfdd68ab1d7713d156cc5bb9d
Miguel Monteiro <miguelmonteiroclaveri@gmail.com>
2019-05-07 11:44:12 +0200

    hemos añadido dependencias lang3

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,16 +1,25 @@
 package clinic.programming.training;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
+ public  int countWords(String words){
+        String[] separateWords = StringUtils.split(words, ' ');
+        if  (separateWords == null){
+            return 0;
+        }else {
+            return separateWords.length;
+        }
+    }
 
     public void greet(){
-	List<String> greetings = new ArrayList<>();
-	greetings.add("hello");
-	for(String greeting : greetings){
-		System.out.println("greeting: " + greeting);
-	}
+        List<String> greetings = new ArrayList<>();
+        greetings.add("hello");
+        for(String greeting : greetings){
+            System.out.println("greeting: " + greeting);
+        }
     }
 
     public Application() {
@@ -19,8 +28,12 @@ public class Application {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
-	app.greet();
+        System.out.println ("Starting Application");
+        Application app = new Application();
+        app.greet();
+        int result = app.countWords("Hola mundo esto con seis palabras");
+        System.out.println("num palabras: " + result);
     }
+   
+
 }
